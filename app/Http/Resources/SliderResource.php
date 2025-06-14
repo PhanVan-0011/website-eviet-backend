bcc8b8e8367d998d96d7cd435642498bde366f91
Phan Van Van <pvvan@liink.vn>
2025-06-14 17:01:37 +0700

    Update Slider

--- app/Http/Resources/SliderResource.php
+++ app/Http/Resources/SliderResource.php
@@ -4,6 +4,12 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Product;
+use App\Models\Combo;
+use App\Models\Post;
+use App\Http\Resources\ProductResource;
+use App\Http\Resources\ComboResource;
+use App\Http\Resources\PostResource;
 
 class SliderResource extends JsonResource
 {
@@ -19,12 +25,30 @@ class SliderResource extends JsonResource
             'title' => $this->title,
             'description' => $this->description,
             'image_url' => $this->image_url,
-            'link_url' => $this->link_url,
+
             'display_order' => $this->display_order,
             'is_active' => $this->is_active,
-            'link_type' => $this->link_type,
-            'combo_id' => $this->combo_id,
-            'combo' => new ComboResource($this->whenLoaded('combo')),
+
+            'linkable_id' => $this->linkable_id,
+            'linkable_type' => $this->linkable_type,
+            'linkable_data' => $this->whenLoaded('linkable', function () {
+                $linkable = $this->linkable;
+
+                if ($linkable instanceof Product) {
+                    // ProductResource
+                    return new ProductResource($linkable);
+                }
+                if ($linkable instanceof Combo) {
+                    //ComboResource
+                    return new ComboResource($linkable);
+                }
+                if ($linkable instanceof Post) {
+                    //PostResource
+                    return new PostResource($linkable);
+                }
+                return null;
+            }),
+
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
